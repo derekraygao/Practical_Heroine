06da5dcd8e08a473e7f25b3b54b44afc617dc1c8
Derek <derekraygao@gmail.com>
2021-07-12 18:32:48 -0700

    PP2: Marcus created

---
+++ frontend/src/components/GamePlayBoxComponents/ActionBoxComponents/GamePhase5/PowerPhase5Components/Marcus/PP5Marcus.js
@@ -0,0 +1,108 @@
+import React from 'react';
+import { connect } from 'react-redux';
+
+import './PP5Marcus.css';
+
+import {updateCharacterPowerHistory} from 'actions/updateCharacterPowerHistory.js';
+import {systemMessages} from 'actions/systemMessages.js';
+import socket from 'Socket.js';
+
+
+class PP5Marcus extends React.Component {
+
+
+
+  submitButtonClick = () => {
+
+    this.props.updatePower("Marcus", 
+                           "berserkMissionNo", 
+                           this.props.missionNumber
+                          );
+
+    socket.emit("Activate Berserk");
+
+  };
+
+
+
+
+  render() {
+
+    if (this.props.PH["Marcus"].berserkMissionNo !== 0) {
+
+      return (
+
+        <div className="PP5-general-container">
+
+          <div className="PP5-powers-menu-bar-container orange ui buttons">
+            <button className="ui button"></button>
+          </div> 
+
+          <div className="PP5-general-action-area-container">
+            You used the one-time terrifying Dark elemental 
+            ability of "Berserk" during Mission  
+            &nbsp;{this.props.PH["Marcus"].berserkMissionNo}.
+          </div>
+
+        </div>
+
+
+      ); //end return
+
+    }; //end if PH.berserk = true
+
+
+
+    return (
+
+      <div className="PP5-general-container">
+
+        <div className="PP5-powers-menu-bar-container orange ui buttons">
+          <button className="ui button">Berserk</button>
+        </div> 
+
+        <div className="PP5-general-action-area-container">
+
+          <button
+            className="ui button yellow"
+            onClick={this.submitButtonClick}
+          >
+            Berserk!
+          </button>
+
+        </div>
+
+      </div>
+
+
+    ); //end return
+
+  }; //end render()
+
+
+
+}; //end class PP5Marcus
+
+
+
+const mapStateToProps = (state) => {
+  
+  return (
+         {  
+            PH: state.characterPowersHistory,
+            missionNumber: state.missionNumber,
+            role: state.role,
+            timer: state.timer
+         }
+  );
+
+};
+
+
+export default connect(mapStateToProps, 
+  {
+    updatePower: updateCharacterPowerHistory,
+    addSystemMessage: systemMessages,
+  }
+)
+(PP5Marcus);
