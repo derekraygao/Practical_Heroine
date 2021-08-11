341658034dd3f7b9fdb2aaf5ea1bc766e4490433
Derek <derekraygao@gmail.com>
2021-08-11 14:46:30 -0700

    moved redux store from index.js to outside file - so I can access store in non-react components, maybe like for dynamic socket.io connections

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
 
-import reducers from './reducers/index.js';
+
+import store from './store.js';
 import App from './components/App';
+import Lobby from './components/Lobby/Lobby.js';
+import Home from './components/Home.js';
 
 ReactDOM.render(
 
-	<Provider store={createStore(reducers)}>
-     <App />
+	<Provider store={store}>
+     <Home />
 	</Provider>
   , 
   document.querySelector('#root')
