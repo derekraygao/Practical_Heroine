16c813a920f8e3b168d587b5e6f0e709e3391efa
Derek <derekraygao@gmail.com>
2021-08-26 16:31:25 -0700

    Revert "testing new player & results box layout"
    This reverts commit 62632ab74b5bf71f31a42ee19d6a6c8d7be728ca.

--- frontend/src/components/MenuBarBoxComponents/PlayerAndResultsBox.js
+++ frontend/src/components/MenuBarBoxComponents/PlayerAndResultsBox.js
@@ -6,9 +6,6 @@ import './css/PlayerAndResultsBox.css';
 import PlayerListTable from './PlayerAndResultsTables/PlayerListTable.js';
 import MissionAndTeamResultsTable from './PlayerAndResultsTables/MissionAndTeamResultsTable.js';
 
-
-
-
 class PlayerAndResultsBox extends React.Component {
 
   state = {
@@ -21,30 +18,15 @@ class PlayerAndResultsBox extends React.Component {
   }; //end componentDidMount
 
 
-
-  /*need this since position:fixed inherits width/height of viewport
-  and no longer the parent div*/
-  rulesAndInfoMenuWidth = () => {
-
-    if (this.props.gamePhase === 8) {
-      return {width: "40%"};
-    };
-
-    return {width: "60%"};
-
-  };
-
-
-
   prBoxStyle = () => {
 
     if (this.props.mainMenuSelection === "player & results") {
 
       if (this.props.gamePhase === 8) {
-        return {display: "grid", gridArea: "other-menu-box"};
+        return {display: "flex", gridArea: "other-menu-box"};
       };
 
-      return {display: "grid", gridArea: "chat-box"};
+      return {display: "flex", gridArea: "chat-box"};
 
     }; //end if mainMenuSelection === notes
 
@@ -59,13 +41,8 @@ class PlayerAndResultsBox extends React.Component {
 
       <div class="player-and-results-box-container" style={this.prBoxStyle()}>
 
-        <div className="player-order-fixed-position-div" style={this.rulesAndInfoMenuWidth()}> 
-          <span> <PlayerListTable /> </span>
-        </div>
-
-        <div className="results-box-container">
-          <MissionAndTeamResultsTable />
-        </div>
+        <PlayerListTable />
+        <MissionAndTeamResultsTable />
 
       </div>
 
